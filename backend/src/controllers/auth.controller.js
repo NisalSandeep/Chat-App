68c449a4cdb5902cd293ea335f11e89ce4df3aa0
NisalSandeep <sandeep-as22018@stu.kln.ac.lk>
2026-03-08 16:37:54 +0530

    welcome email implement

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -2,10 +2,11 @@ import bcrypt from "bcryptjs";
 
 import User from "../models/User.js";
 import { generateToken } from "../lib/utils.js";
+import { sendWelcomeEmail } from "../emails/emailHandlers.js";
+import { ENV } from "../lib/env.js";
 
 export const signup = async (req, res) => {
   const { fullName, email, password } = req.body;
-
 
   try {
     if (!fullName || !email || !password) {
@@ -39,17 +40,25 @@ export const signup = async (req, res) => {
     });
 
     if (newUser) {
-      generateToken(newUser._id, res);
-      await newUser.save();
+
+      const savedUser = await newUser.save();
+      generateToken(savedUser._id, res);
 
       res.status(201).json({
-        _id: newUser._id,
-        fullName: newUser.fullName,
-        email: newUser.email,
-        profilePic: newUser.profilePic,
+        _id: savedUser._id,
+        fullName: savedUser.fullName,
+        email: savedUser.email,
+        profilePic: savedUser.profilePic,
       });
 
       //todo: send a welcome email to user
+
+      try {
+          await sendWelcomeEmail(savedUser.email, savedUser.fullName,ENV.CLIENT_URL );
+      }catch(error) {
+        console.log("failed to send Email");
+      }
+
     } else {
       res.status(400).json({ message: "Invalid user data" });
     }
