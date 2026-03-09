048af995cdfade193258f6eea8cd4c7067f82c28
NisalSandeep <sandeep-as22018@stu.kln.ac.lk>
2026-03-09 10:45:23 +0530

    update profile implement

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import path from "path";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
@@ -16,6 +17,7 @@ const __dirname = path.resolve();
 const PORT = ENV.PORT || 3000;
 
 app.use(express.json()) //req.body
+app.use(cookieParser()) //req.cookies;
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
