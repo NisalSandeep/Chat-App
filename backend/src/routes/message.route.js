a76c9f3ac5453d3a4ef013de14265ec704ab3dda
NisalSandeep <sandeep-as22018@stu.kln.ac.lk>
2026-03-10 09:36:13 +0530

    messages-implement

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,10 +1,17 @@
 import express from "express";
+import { getAllContacts, getMessagesByUserId, sendMessage, getChatPartners } from "../controllers/message.controller.js";
+import {protectRoute} from "../middleware/auth.middleware.js"
+import {arcjectProtection} from "../middleware/arcject.middleware.js";
 
 
-const router = express.Router();
+const   router = express.Router();
 
-router.get("/send", (req, res) => {
-    res.send("send message endpoint");
-});
+router.use(arcjectProtection, protectRoute) //these middlewares run by order
+
+
+router.get("/contacts", getAllContacts);
+router.get("/chat", getChatPartners);
+router.get("/:id", getMessagesByUserId);
+router.post("/send/:id", sendMessage );
 
 export default router;
