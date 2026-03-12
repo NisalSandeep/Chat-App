4e56aa5bd73448790c8f61db0678ffdd5397208e
NisalSandeep <sandeep-as22018@stu.kln.ac.lk>
2026-03-12 18:43:12 +0530

    login-logout-implement

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -6,16 +6,35 @@ export const useAuthStore = create((set) => ({
   authUser: null,
   isCheckingAuth: true,
   isSigningUp: false,
+  isLoginIn: false,
+
 
   checkAuth: async () => {
     try {
       const res = await axiosInstance.get("/auth/check");
       set({ authUser: res.data });
     } catch (error) {
-      console.log("Error in authCheck: ", error);
+      // 401 is expected when not logged in
+      if (error.response?.status !== 401) {
+        console.log("Error in authCheck: ", error);
+      }
       set({ authUser: null });
     } finally {
       set({ isCheckingAuth: false });
+    }
+  },
+
+  login: async (data) => {
+    set({ isLoginIn: true });
+    try {
+      const res = await axiosInstance.post("/auth/login", data);
+      set({ authUser: res.data });
+
+      toast.success("Logged in successfully");
+    } catch (error) {
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isLoginIn: false });
     }
   },
 
@@ -32,4 +51,16 @@ export const useAuthStore = create((set) => ({
       set({ isSigningUp: false });
     }
   },
+
+  logout: async () => {
+    
+    try {
+      await axiosInstance.post("/auth/logout");
+      set({authUser: null});
+
+      toast.success("Logout successfully");
+    }catch (error) {
+      toast.error("Failed to logout");
+    }
+  }
 }));
