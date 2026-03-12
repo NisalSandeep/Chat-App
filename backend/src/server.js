937d40a66b2ee6a005d0fe5b2e9c4242a402e217
NisalSandeep <sandeep-as22018@stu.kln.ac.lk>
2026-03-12 15:50:39 +0530

    signup implemntation on the frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,6 +7,7 @@ import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import connectDB from "./lib/db.js";
 import { ENV } from "./lib/env.js";
+import cors from "cors";
 
 dotenv.config();
 
@@ -16,8 +17,9 @@ const __dirname = path.resolve();
 
 const PORT = ENV.PORT || 3000;
 
-app.use(express.json()) //req.body
-app.use(cookieParser()) //req.cookies;
+app.use(express.json()); //req.body
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+app.use(cookieParser()); //req.cookies;
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
